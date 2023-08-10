ead642292ce0e7526df3f3d468fb7b4e146ca39d
Pury-01 <puryokumu@gmail.com>
2023-08-10 08:38:51 -0700

    C program that prints size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main-Entry point
+*Return:Always 0 (success)
+*/
+int main(void)
+{
+printf(" Size of(char)\n");
+printf(" Size of(short)\n");
+printf(" Size of(int)\n");
+printf(" Size of(long)\n");
+printf(" Size of(long long)\n");
+printf(" Size of(float)\n");
+printf(" Size of(double)\n");
+printf(" size of(long double)\n");
+
+return (0);
+}

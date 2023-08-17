f55c25d295ab45727c822712629d72ed4e128b5e
Pury-01 <puryokumu@gmail.com>
2023-08-17 07:54:15 -0700

    prototypes

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,10 +4,10 @@
 int _putchar(char c);
 void print_alphabet(void);
 void print_aphabet_x10(void);
-int_islower(int c);
-int_isalpha(int c);
+int _islower(int c);
+int _isalpha(int c);
 int print_sign(int n);
-int_abs(int);
+int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);

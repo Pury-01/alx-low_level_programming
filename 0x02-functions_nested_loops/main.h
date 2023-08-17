134afe8f0cdad76ea6c8e4e2107041a058c0fc80
Pury-01 <puryokumu@gmail.com>
2023-08-17 07:07:52 -0700

    prototype file main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef main_h
 #define main_h
 
+int _putchar(char c);
 void print_alphabet(void);
 void print_aphabet_x10(void);
 int_islower(int c);
@@ -12,6 +13,7 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int  n);
+void print_times_table(int n);
 
 #endif
 

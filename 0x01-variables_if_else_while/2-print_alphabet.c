aa1386c6991322ad52bf7b85ffc0408d24c28e17
Pury-01 <puryokumu@gmail.com>
2023-08-19 11:24:50 -0700

    print alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,10 +10,10 @@ int main(void)
 	char alphabet = 'a';
 
 	while (alphabet <= 'z')
-{
-	putchar(alphabet);
-	alphabet++;
-}
-putchar('\n');
-return (0);
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
 }

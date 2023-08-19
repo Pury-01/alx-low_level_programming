01ce660a5dcc00124dcde548bad2ee653e8a3bf4
Pury-01 <puryokumu@gmail.com>
2023-08-19 11:32:27 -0700

    alphabets in lowercase followed by new line

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,25 +4,25 @@
  *main - Entry point
  *print alphabets in lowercase
  *exempt q and e
- *Return : Always 0(success)
+ *Return: Always 0(success)
  */
 
 int main(void)
 {
-int lowercase  = 'a';
+	int lowercase  = 'a';
 
-while (lowercase <= 'z')
-{
-if (lowercase == 'q' || lowercase == 'e')
-{
-lowercase  += 1;
-}
-else
-{
-putchar(lowercase);
-lowercase += 1;
-}
-}
-putchar('\n');
-return (0);
+	while (lowercase <= 'z')
+	{
+		if (lowercase == 'q' || lowercase == 'e')
+		{
+			lowercase  += 1;
+		}
+		else
+		{
+			putchar(lowercase);
+			lowercase += 1;
+		}
+	}
+	putchar('\n');
+	return (0);
 }

ba60ce192714d6fbddc69f7e8229fd834f770f0f
Pury-01 <puryokumu@gmail.com>
2023-08-19 00:15:49 -0700

    numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,19 +3,18 @@
 /**
 *main -Entry
 *Return : Always 0(success)
-*this program print single-digit numbers 
+*this program print single-digit numbers
 */
 
 int main(void)
 {
-  int number = 0;
+int number;
 
 while (number < 10)
 {
-   putchar('0' + number);
-   number++;
+printf("%d", number);
+number += 1;
 }
-   putchar('\n'0;
-		 
+printf("\n");
 return (0);
 }

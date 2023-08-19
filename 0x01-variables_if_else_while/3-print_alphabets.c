10b1e55cd763f3e4ec1abb06772569101449d882
Pury-01 <puryokumu@gmail.com>
2023-08-19 11:29:38 -0700

    lowercase then uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,29 +5,30 @@
  *This program prints alphabets
  *first prints in lowercase
  *then converts them to uppercase
+ *Return: Always 0(success)
  */
 int main(void)
 {
 	char lowercase = 'a';
 	char uppercase = 'A';
 
-/*print lowercase alphabets*/
+	/*print lowercase alphabets*/
 
-while (lowercase <= 'z')
-{
-	putchar(lowercase);
-	lowercase++;
-}
-/*print uppercase alphabets*/
+	while (lowercase <= 'z')
+	{
+		putchar(lowercase);
+		lowercase++;
+	}
+	/*print uppercase alphabets*/
 
-while (uppercase <= 'Z')
-{
-	putchar(uppercase);
-	uppercase++;
-}
-/*print new line*/
+	while (uppercase <= 'Z')
+	{
+		putchar(uppercase);
+		uppercase++;
+	}
+	/*print new line*/
 
-putchar('\n');
+	putchar('\n');
 
-return (0);
+	return (0);
 }

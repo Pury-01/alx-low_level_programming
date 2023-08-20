e31efe1bf5adb04b107cb4bf02af853afe757922
Pury-01 <puryokumu@gmail.com>
2023-08-20 08:42:52 -0700

    checks for alphabetic character

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *print_sign - prints sign of a number
+ *Return: 1 when +ve 0 when zero -1 when -ve
+ */
+
+int print_sign(int n)
+{
+	n = 0;
+	while (n <= 0)
+	{
+		for (n < 0)
+		{
+			_putchar ('-');
+			return (-1);
+		}
+		{
+			for (n ==0)
+			{
+				_putchar('0');
+				return (0);
+			}
+		}
+	}
+	return (1);
+}
+

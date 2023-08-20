54ab15d38f799c2585362fa804a6306b191f3bb8
Pury-01 <puryokumu@gmail.com>
2023-08-20 07:28:15 -0700

    10 times the alphabet, in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,14 +12,14 @@ void print_alphabet_x10(void)
 	int i;
 
 	i = 0;
-
-	while (i < 10) 
+	while (i < 10)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 		{
 			_putchar(ch);
 		}
 		_putchar('\n');
+		i++;
 
 	}
 }

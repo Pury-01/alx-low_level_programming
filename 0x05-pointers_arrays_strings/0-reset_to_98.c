dfca6f6368c83b8270890f0c39e53d7682c3faba
Pury-01 <puryokumu@gmail.com>
2023-08-21 07:42:46 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - this takes the pointer to an integer as  a parameter
+ *it then updates the value it points to to 98
+ *Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+This project is on c pointers, arrays and strings
+It covers memory allocation of variables, pointers and arrays;length of strings among others

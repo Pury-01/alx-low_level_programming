492090b2ca4731b408e097f6ab67690fcc11d95f
root <root@pury.localdomain>
2023-08-31 23:02:40 +0300

    function that checks for prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,29 +3,29 @@
 /**
  *is_prime_number - checks if interger is a prime number
  *@n: the number to be checked
+ *is_prime_helper - called help find prime numbers
+ *@i: divisor used to determine a prime number
  *Return: 1 if integer is prime number, else 0
  */
 
 int is_prime_helper(int n, int i)
 {
-	if (i * i > n) 
+	if (i * i > n)
 	{
 		return (1);
 	}
-	if (n % i == 0) 
+	if (n % i == 0)
 	{
 		return (0);
 	}
-
-
-	return (is_prime_helper(n, i+ 1));
+	return (is_prime_helper(n, i + 1));
 }
 
 int is_prime_number(int n)
 {
-	if (n <= 1) 
+	if (n <= 1)
 	{
-		return (0); 
+		return (0);
 	}
 	return (is_prime_helper(n, 2));
 }

d06599d6a3ae1985f7779406c772ec8b83aeb55e
Pury-01 <puryokumu@gmail.com>
2023-10-31 22:30:07 +0300

    program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,7 +16,7 @@ void check_file(int file_out, int file_in, char *argv[])
 	if (file_in == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(98);
+		exit(99);
 	}
 }
 
